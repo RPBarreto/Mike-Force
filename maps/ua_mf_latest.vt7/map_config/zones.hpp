15958c0e034dc343712b8a242cb6256944be33fe
SirBlueHood <109435060+SirBlueHood@users.noreply.github.com>
2022-09-10 23:26:39 -0700

    Updated zones.hpp

--- maps/ua_mf_latest.vt7/map_config/zones.hpp
+++ maps/ua_mf_latest.vt7/map_config/zones.hpp
@@ -1,111 +1,168 @@
-zone_ban_hoang[] = {
-	{"zone_hue", "connector_ban_hoang_hue"},
+/*Below is the template for creating a zone.
+zone_ban_hoang[] = { "This line is for the name of the zone beggening tieh the prefix 'zone'."
+	{"zone_hue", "connector_ban_hoang_hue"}, "All other lines are for creating connectors between zones which guide the enem assualt and reinforcement scripts."
 	{"zone_saigon", "connector_ban_hoang_saigon"},
 	{"zone_da_nang", "connector_ban_hoang_da_nang"}
+};*/
+
+zone_nopala[] = {
+	{}
 };
 
-zone_ba_ria[] = {
-	{"zone_saigon", "connector_ba_ria_saigon"},
-	{"zone_cu_chi", "connector_ba_ria_cu_chi"}
+zone_piispa[] = {
+	{}
 };
 
-zone_saigon[] = {
-	{"zone_can_tho", "connector_saigon_can_tho"},
-	{"zone_soc_trang", "connector_saigon_soc_trang"}
+zone_leviakallio[] = {
+	{}
 };
 
-zone_soc_trang[] = {
-	{"zone_phu_quoc", "connector_soc_trang_phu_quoc"},
-	{"zone_fsb_quan_loi", "connector_soc_trang_quan_loi"}
+zone_suuretvuoret[] = {
+	{}
 };
 
-zone_cu_chi[] = {
-	{"zone_can_tho", "connector_cu_chi_can_tho"},
-	{"zone_a_shau_valley", "connector_cu_chi_a_shau_valley"}
+zone_suopelto[] = {
+	{}
 };
 
-zone_can_tho[] = {
-	{"zone_fsb_quan_loi", "connector_can_tho_quan_loi"}
+zone_pirunsarvi[] = {
+	{}
 };
 
-zone_phu_quoc[] = {
-	{"zone_lumphat", "connector_phu_quoc_lumphat"},
-	{"zone_dharma", "connector_phu_quoc_dharma"}
+zone_valkjarvi[] = {
+	{}
 };
 
-zone_lumphat[] = {
-	{"zone_phu_quoc", "connector_lumphat_phu_quoc"},
-	{"zone_attapeu", "connector_lumphat_attapeu"},
-	{"zone_sihanoukville", "connector_lumphat_sihanoukville"},
-	{"zone_san_bay_ban", "connector_lumphat_san_bay_ban"}
+zone_haavisto[] = {
+	{}
 };
 
-zone_fsb_quan_loi[] = {
-	{"zone_lumphat", "connector_quan_loi_lumphat"},
-	{"zone_a_shau_valley", "connector_quan_loi_a_shau_valley"}
+zone_kannanmaki_west[] = {
+	{}
 };
 
-zone_a_shau_valley[] = {
-	{"zone_fsb_quan_loi", "connector_a_shau_valley_quan_loi"},
-	{"zone_da_krong", "connector_a_shau_valley_khe_sanh"},
-	{"zone_khe_gio_valley", "connector_a_shau_valley_khe_gio_valley"}
+zone_kannanmaki_east[] = {
+	{}
 };
 
-zone_khe_gio_valley[] = {
-	{"zone_hue", "connector_khe_gio_valley_hue"},
-	{"zone_da_krong", "connector_khe_gio_valley_khe_sanh"},
-	{"zone_hai_phong", "connector_khe_gio_valley_hanoi_docks"}
+zone_uutela[] = {
+	{}
 };
 
-zone_hai_phong[] = {
-	{"zone_hanoi_city", "connector_hanoi_docks_hanoi_city"}
+zone_yla_pihlaja[] = {
+	{}
 };
 
-zone_hanoi_city[] = {
-	{"zone_gia_lam_airbase", "connector_hanoi_city_gia_lam_airbase"},
-	{"zone_ha_long_navy_base", "connector_hanoi_city_ha_long_navy_base"}
+zone_kuusela[] = {
+	{}
 };
 
-zone_hue[] = {
-	{"zone_khe_gio_valley", "connector_hue_khe_gio_valley"},
-	{"zone_fob_mai_loc", "connector_hue_fob_mai_loc"}
+zone_lansikyla[] = {
+	{}
 };
 
-zone_da_krong[] = {
-	{"zone_attapeu", "connector_khe_sanh_attapeu"},
-	{"zone_bru_village", "connector_khe_sanh_bru_village"}
+zone_klamila[] = {
+	{}
 };
 
-zone_attapeu[] = {
-	{"zone_plain_of_jars", "connector_attapeu_plain_of_jars"},
-	{"zone_mu_gia_pass", "connector_attapeu_mu_gia_pass"}
+zone_jarvenkyla[] = {
+	{}
 };
 
-zone_mu_gia_pass[] = {
-	{"zone_va_cang", "connector_mu_gia_pass_va_cang"},
-	{"zone_thud_ridge", "connector_mu_gia_pass_thud_ridge"}
+zone_kelkkaniemi[] = {
+	{}
 };
 
-zone_va_cang[] = {
-	{"zone_nodal_lho", "connector_va_cang_nodal_lho"}
+zone_harju[] = {
+	{}
 };
 
-zone_thud_ridge[] = {
-	{"zone_son_tay", "connector_thud_ridge_son_tay"}
+zone_tinkanen[] = {
+	{}
 };
 
-zone_ha_long_navy_base[] = {
-	{"zone_son_tay", "connector_ha_long_navy_base_son_tay"},
-	{"zone_ling_ho", "connector_ling_ho"}
+zone_santasaari[] = {
+	{}
 };
 
-zone_dharma[] = {
-	{"zone_sihanoukville", "connector_dharma_sihanoukville"}
+zone_siikasaari[] = {
+	{}
 };
 
-zone_sihanoukville[] = {
-	{"zone_dharma", "connector_sihanoukville_dharma"},
-	{"zone_san_bay_ban", "connector_sihanoukville_san_bay_ban"}
+zone_hanski[] = {
+	{}
+};
+
+zone_niemela[] = {
+	{}
+};
+
+zone_ravijoki[] = {
+	{}
+};
+
+zone_hill_43[] = {
+	{}
+};
+
+zone_ala_pihlaja[] = {
+	{}
+};
+
+zone_kirkonkyla[] = {
+	{}
+};
+
+zone_eerikkala[] = {
+	{}
+};
+
+zone_selkarannankangas[] = {
+	{}
+};
+
+zone_hurppu[] = {
+	{}
+};
+
+zone_vilkkila[] = {
+	{}
+};
+
+zone_quarry_east[] = {
+	{}
+};
+
+zone_pyterlahti[] = {
+	{}
+};
+
+zone_ylapaa[] = {
+	{}
+};
+
+zone_myllynmaki[] = {
+	{}
+};
+
+zone_virojoki[] = {
+	{}
+};
+
+zone_rus_airbase[] = {
+	{}
+};
+
+zone_rus_outpost_1[] = {
+	{}
+};
+
+zone_industrial_park[] = {
+	{}
+};
+
+zone_pajunlahti[] = {
+	{}
 };
 
 zone_san_bay_ban[] = {};
